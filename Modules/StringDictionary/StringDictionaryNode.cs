bbaf50b4c38d258973bb16f600fbd22e3d283247
cloneproduction <clone@cloneproduction.net>
2018-07-12 23:18:36 +0800

    Changed Dictionary for OSC Receivers

--- Modules/StringDictionary/StringDictionaryNode.cs
+++ Modules/StringDictionary/StringDictionaryNode.cs
@@ -18,81 +18,57 @@ namespace VVVV.Nodes
 
 	public class StringDictionaryNode : IPluginEvaluate
 	{
-		#region fields & pins
+        #region fields & pins
 
-        [Input("Add", IsBang = true)]
-        public IDiffSpread<bool> FAdd;
-
-        [Input("Add Key", DefaultString = "")]
-        public ISpread<string> FAddKey;
-
-        [Input("Add Value", DefaultString = "")]
-        public IDiffSpread<ISpread<string>> FAddValue;
-
+        [Input("Value", DefaultString = "")]
+        public IDiffSpread<ISpread<string>> FValue;
 
         [Input("Update")]
         public ISpread<bool> FUpdate;
 
-        [Input("Update Key", DefaultString = "")]
-        public ISpread<string> FUpdateKey;
-
-        [Input("Update Value", DefaultString = "")]
-        public IDiffSpread<ISpread<string>> FUpdateValue;
-
-
-        [Input("Delete", IsBang = true)]
-        public IDiffSpread<bool> FDelete;
+        [Input("Key")]
+        public ISpread<string> FKey;
 
         [Input("Delete Key")]
         public IDiffSpread<string> FDeleteKey;
-
-
-        [Input("Output Key" )]
-        public IDiffSpread<string> FQueryKey;
-
 
         [Output("Output")]
 		public ISpread<ISpread<string>> FOutputQueryValue;
 
         #endregion fields & pins
+
         Dictionary<string, ISpread<string>> dict = new Dictionary<string, ISpread<string>>();
 
 
         public void Evaluate(int SpreadMax)
 		{
-            if (FAdd.IsChanged)
+            if (FKey.IsChanged && FKey.SliceCount != 0)
             {
-                if(FAddValue.SliceCount != 0 && FAddKey.SliceCount != 0)
+                for (int i = 0; i < FKey.SliceCount; i++)
                 {
-                    for (int i = 0; i < FAddKey.SliceCount; i++)
+                    if (!dict.ContainsKey(FKey[i]))
                     {
-                        if (!FAdd[i]) continue;
-                        if (dict.ContainsKey(FAddKey[i]) == false)
+                        dict.Add(FKey[i], FValue[i].Clone());
+                    }
+                }
+            }
+
+            if (FUpdate.IsChanged && FUpdate.SliceCount != 0)
+            {
+                for (int i = 0; i < FKey.SliceCount; i++)
+                {
+                    if (FUpdate[i])
+                    {
+                        if (dict.ContainsKey(FKey[i]))
                         {
-                            dict.Add(FAddKey[i], FAddValue[i].Clone());
+                            dict[FKey[i]] = FValue[i].Clone();
                         }
                     }
                 }
             }
 
-            if (FUpdate.IsChanged)
-            {
-                if (FUpdateKey.SliceCount != 0 && FUpdateValue.SliceCount != 0)
-                {
-                    for (int i = 0; i < FUpdateKey.SliceCount; i++)
-                    {
-                        if (FUpdate[i])
-                        {
-                            if (dict.ContainsKey(FUpdateKey[i]))
-                            {
-                                dict[FUpdateKey[i]] = FUpdateValue[i].Clone();
-                            }
-                        }
-                    }
-                }
-            }
 
-            if (FDelete.IsChanged && FDeleteKey.SliceCount != 0)
+            if (FDeleteKey.IsChanged && FDeleteKey.SliceCount != 0)
             {
                 for (int i = 0; i < FDeleteKey.SliceCount; i++)
                 {
@@ -103,27 +79,27 @@ namespace VVVV.Nodes
                 }
             }
 
-            FOutputQueryValue.SliceCount = FQueryKey.SliceCount;
+            FOutputQueryValue.SliceCount = FKey.SliceCount;
 
-            for (int i = 0; i < FQueryKey.SliceCount; i++)
+            for (int i = 0; i < FKey.SliceCount; i++)
             {
-                if (FQueryKey.IsChanged || FDeleteKey.IsChanged || FUpdate.IsChanged || FAdd.IsChanged || FDelete.IsChanged)
+                if (FKey.IsChanged || FDeleteKey.IsChanged || FUpdate.IsChanged)
                 {
-                    if (FQueryKey[i] == null)
+                    if (FKey[i] == null)
                     {
                         FOutputQueryValue[i].SliceCount = 0;
                         continue;
                     }
 
-                    if (!dict.ContainsKey(FQueryKey[i]))
+                    if (!dict.ContainsKey(FKey[i]))
                     {
                         FOutputQueryValue[i].SliceCount = 0;
                         continue;
                     }
 
-                    if (dict.ContainsKey(FQueryKey[i]))
+                    if (dict.ContainsKey(FKey[i]))
                     {
-                        FOutputQueryValue[i] = dict[FQueryKey[i]];
+                        FOutputQueryValue[i] = dict[FKey[i]];
                     }
                 }
             }
